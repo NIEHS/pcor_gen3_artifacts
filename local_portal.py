02327aff693491b28a5fa785ca74e4fc77b1f077
Mike Conway <Michael.c.conway@gmail.com>
2023-02-06 12:54:38 -0500

    add local portal python script

---
+++ local_portal.py
@@ -0,0 +1,42 @@
+"""
+This script will start the data portal locally in development mode using 
+the configured gitops and schema
+
+"""
+import json
+import os
+import sys
+import logging
+import argparse
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s: %(filename)s:%(funcName)s: %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+def run_local(portal_dir, cdismanifest, pcor_artifacts):
+    logging.info("run local")
+    logging.info("portal_dir:%s" % portal_dir)
+    logging.info("cdismanifest:%s" % cdismanifest)
+    logging.info("pcor_artifacts:%s" % pcor_artifacts)
+
+def main():
+    logging.info("startup")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataportal", help="local parent directory for the data portal repo")
+    parser.add_argument("--cdismanifest", help="local parent directory for cdis manfiest")
+    parser.add_argument("--pcorartifacts", help="local parent directory for pcorartifacts")
+
+    args = parser.parse_args()
+
+   
+    portal_dir = args.dataportal
+    cdismanifest = args.cdismanifest
+    pcor_artifacts = args.pcorartifacts
+    run_local(portal_dir, cdismanifest, pcor_artifacts)
+
+
+if __name__ == "__main__":
+    main()
