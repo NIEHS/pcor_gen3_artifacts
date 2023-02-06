b0104dccfe55ec68a863b434fb392c8d7e09d11b
Mike Conway <Michael.c.conway@gmail.com>
2023-02-06 14:20:34 -0500

    working on run now

--- local_portal.py
+++ local_portal.py
@@ -20,37 +20,42 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def run_local(portal_dir, pcor_artifacts, cdismanifest=LOCAL_PORTAL_DNS_NAME):
+def setup_and_run(portal_dir, pcor_artifacts, cdismanifest=LOCAL_PORTAL_DNS_NAME, runit=False):
     logging.info("run local")
     logging.info("portal_dir:%s" % portal_dir)
     logging.info("cdismanifest:%s" % cdismanifest)
     logging.info("pcor_artifacts:%s" % pcor_artifacts)
+    logging.info("runit:%s" % runit)
+
 
     # move gitops to portal folder
     shutil.copy2(pcor_artifacts+'/custom_configs/gitops.json', pcor_artifacts + "/" + cdismanifest + "/portal/gitops.json")
 
     
-
 def main():
     logging.info("startup")
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataportal", help="local parent directory for the data portal repo", default="/Users/conwaymc/Documents/workspace-pcor/data-portal")
     parser.add_argument("--cdismanifest", help="local parent directory for cdis manfifest", default=LOCAL_PORTAL_DNS_NAME)
     parser.add_argument("--pcorartifacts", help="local parent directory for pcorartifacts", default="/Users/conwaymc/Documents/workspace-pcor/pcor_gen3_artifacts")
+    parser.add_argument("--run", help="flag indicates that the portal should be started", default=False)
 
     args = parser.parse_args()
-
-   
     portal_dir = args.dataportal
     cdismanifest = args.cdismanifest
     pcor_artifacts = args.pcorartifacts
-
+    runit = args.run
     
-    logger.debug("portal_dir:%s" % portal_dir)
-    logger.debug("cdismanifest:%s" % cdismanifest)
-    logger.debug("pcor_artifacts:%s" % pcor_artifacts)
+    logger.info("portal_dir:%s" % portal_dir)
+    logger.info("cdismanifest:%s" % cdismanifest)
+    logger.info("pcor_artifacts:%s" % pcor_artifacts)
+    logger.info("runit:%s" % runit)
 
-    run_local(portal_dir=portal_dir, pcor_artifacts=pcor_artifacts, cdismanifest=cdismanifest)
+    setup_and_run(portal_dir=portal_dir, pcor_artifacts=pcor_artifacts, cdismanifest=cdismanifest, runit=runit)
+
+    if not runit:
+        logger.info("not running, all done!")
+        exit(0)
 
 
 if __name__ == "__main__":
