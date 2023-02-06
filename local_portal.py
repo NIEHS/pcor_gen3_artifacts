9257f98210e3e4f841c92c36df21d6ed7cfd303c
Mike Conway <Michael.c.conway@gmail.com>
2023-02-06 13:55:47 -0500

    build gitops for deploy

--- local_portal.py
+++ local_portal.py
@@ -8,6 +8,9 @@ import os
 import sys
 import logging
 import argparse
+import shutil
+
+LOCAL_PORTAL_DNS_NAME = "pcor.local"
 
 logging.basicConfig(
     level=logging.INFO,
@@ -16,19 +19,24 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-def run_local(portal_dir, cdismanifest, pcor_artifacts):
+
+def run_local(portal_dir, pcor_artifacts, cdismanifest=LOCAL_PORTAL_DNS_NAME):
     logging.info("run local")
     logging.info("portal_dir:%s" % portal_dir)
     logging.info("cdismanifest:%s" % cdismanifest)
     logging.info("pcor_artifacts:%s" % pcor_artifacts)
+
+    # move gitops to portal folder
+    shutil.copy2(pcor_artifacts+'/custom_configs/gitops.json', pcor_artifacts + "/" + cdismanifest + "/portal/gitops.json")
+
     
 
 def main():
     logging.info("startup")
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataportal", help="local parent directory for the data portal repo")
-    parser.add_argument("--cdismanifest", help="local parent directory for cdis manfiest")
-    parser.add_argument("--pcorartifacts", help="local parent directory for pcorartifacts")
+    parser.add_argument("--dataportal", help="local parent directory for the data portal repo", default="/Users/conwaymc/Documents/workspace-pcor/data-portal")
+    parser.add_argument("--cdismanifest", help="local parent directory for cdis manfifest", default=LOCAL_PORTAL_DNS_NAME)
+    parser.add_argument("--pcorartifacts", help="local parent directory for pcorartifacts", default="/Users/conwaymc/Documents/workspace-pcor/pcor_gen3_artifacts")
 
     args = parser.parse_args()
 
@@ -41,6 +49,8 @@ def main():
     logger.debug("portal_dir:%s" % portal_dir)
     logger.debug("cdismanifest:%s" % cdismanifest)
     logger.debug("pcor_artifacts:%s" % pcor_artifacts)
+
+    run_local(portal_dir=portal_dir, pcor_artifacts=pcor_artifacts, cdismanifest=cdismanifest)
 
 
 if __name__ == "__main__":
