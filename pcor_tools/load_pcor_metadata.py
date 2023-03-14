4ec972b77c0c48a16cbc0cc9757ffed29c9a5696
Mike Conway <Michael.c.conway@gmail.com>
2023-03-14 10:56:45 -0400

    add test md

--- pcor_tools/load_pcor_metadata.py
+++ pcor_tools/load_pcor_metadata.py
@@ -12,4 +12,42 @@ logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 # a file containing a "guid" column and additional, arbitrary columns to populate
 # into the metadata service
-MANIFEST = "dbgap_extract_guid.tsv"
+MANIFEST = "test1.tsv"
+
+
+def main():
+    loop = get_or_create_event_loop_for_thread()
+
+    auth = Gen3Auth(refresh_file="credentials.json")
+
+    # must provide a str to namespace the metadata from the file in a block in
+    # the metadata service
+    metadata_source = "dbgap"
+
+    # (optional) override default guid parsing behavior
+    def _custom_get_guid_for_row(commons_url, row, lock):
+        """
+        Given a row from the manifest, return the guid to use for the metadata object.
+
+        Args:
+            commons_url (str): root domain for commons where mds lives
+            row (dict): column_name:row_value
+            lock (asyncio.Semaphore): semaphones used to limit ammount of concurrent http
+                connections if making a call to an external service
+
+        Returns:
+            str: guid
+        """
+        return row.get("guid") # OR row.get("some_other_column")
+
+    # (optional) override default guid parsing behavior
+    manifest_row_parsers["guid_for_row"] = _custom_get_guid_for_row
+
+    loop.run_until_complete(
+        metadata.async_ingest_metadata_manifest(
+            auth.endpoint, manifest_file=MANIFEST, metadata_source=metadata_source, auth=auth
+        )
+    )
+
+if __name__ == "__main__":
+    main()
