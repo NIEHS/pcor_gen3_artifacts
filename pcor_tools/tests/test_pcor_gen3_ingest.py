4e5f4c8321ce4a555ae8d7afa56219e7c75469fd
Mike Conway <Michael.c.conway@gmail.com>
2023-04-14 11:35:57 -0400

    working on unit tests

--- pcor_tools/tests/test_pcor_gen3_ingest.py
+++ pcor_tools/tests/test_pcor_gen3_ingest.py
@@ -214,13 +214,14 @@ class TestPcorGen3Ingest(TestCase):
         pcor_ingest = PcorGen3Ingest(pcor_testing_utilities.get_pcor_ingest_configuration())
         program = "NFS"
         project = PcorIntermediateProjectModel()
-        project.project_name = "NFS-2"
+        project.name = "NFS-2"
+        project.short_name = "NFS-2"
         project.project_code = "NFS-2"
         project.project_state = "open"
         project.project_release_date = ""
         project.support_source = "support source1"
         project.support_id = "support id1"
-        project.releasable = True
+        project.releasable = "true"
         project.investigator_name = "Mike Conway"
         project.investigator_affiliation = "NIEHS"
         project.dbgap_accession_number = "NFS-2"
@@ -228,24 +229,27 @@ class TestPcorGen3Ingest(TestCase):
         project.complete = "Complete"
         project.availability_type = "Open"
         project_id = pcor_ingest.create_project("NFS", project)
-        logger.info('Project name: %s is associated with id: %s' % (project.project_name, project_id))
+        logger.info('Project name: %s is associated with id: %s' % (project.name, project_id))
 
         resource = PcorIntermediateResourceModel()
         resource.submitter_id = "NFS-2-RESC-DISC-1"
         resource.resource_id = "NFS-2-RESC-DISC-1"
-        resource.name = "Smoke plume map"
+        resource.name = "Fire and Smoke Map"
+        resource.short_name = "short name"
         resource.resource_type = "data_resource"
-        resource.subject = "AQI - Air Quality Index"
-        resource.keywords = ["fire", "smoke", "aqi", "wildfire", "plume"]
-        resource.update_frequency = "hourly"
-        resource.secondary_name = "AirNow Plume Mapping"
+        resource.description = "description"
+        resource.intended_use = "intended use"
+        resource.citation = "citation"
+        resource.is_citizen_collected = "false"
+        resource.has_api = "false"
+        resource.domain = "AQI - Air Quality Index"
+        resource.keywords = ["fire", "smoke", "aqi", "wildfire"]
         resource.license_type = ""
         resource.license_text = ""
         resource.created_datetime = ""
+        resource.update_frequency = "hourly"
         resource.contact = "USFS - contact firesmokemap@epa.gov"
-        resource.description = """The AirNow Fire and Smoke Map provides information that you can use to help protect your health from wildfire smoke. Use this map to see Current particle pollution air quality information for your location; Fire locations and smoke plumes; Smoke Forecast Outlooks, where available; and,Recommendations for actions to take to protect yourself from smoke. These recommendations were developed by EPA scientists who are experts in air quality and health. The Map is a collaborative effort between the U.S. Forest Service (USFS)-led Interagency Wildland Fire Air Quality Response Program and the U.S. Environmental Protection Agency (EPA)."""
         resource.use_agreement = "false"
-        resource.verification_datetime = "null"
         actual = pcor_ingest.create_resource(program, project.dbgap_accession_number, resource)
         resc_id = actual.id
 
@@ -283,7 +287,7 @@ class TestPcorGen3Ingest(TestCase):
         discovery.resource_id = resc_id
         discovery.full_name = resource.name
         discovery.description = resource.description
-        discovery.subject = resource.subject
+        discovery.subject = resource.domain
 
         filter = AdvSearchFilter()
         filter.key = "Resource Type"
