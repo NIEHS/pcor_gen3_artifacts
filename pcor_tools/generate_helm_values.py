74b6c6a979568f568c6dd78bea905be541646cbd
Mike Conway <Michael.c.conway@gmail.com>
2024-01-09 16:00:25 -0500

    update to put css into template

--- pcor_tools/generate_helm_values.py
+++ pcor_tools/generate_helm_values.py
@@ -31,6 +31,11 @@ try:
         gitops_content = gitops_file.read()
         logger.debug('gitops_content: \n%s' % gitops_content)
 
+        # Read the content of gitops.json file
+    with open(os.path.join(custom_configs_path, 'gitops.css'), 'r') as gitops_file:
+        css_content = gitops_file.read()
+        logger.debug('css_content: \n%s' % css_content)
+
     # Read the content of etlMapping.yaml file
     with open(os.path.join(custom_configs_path, 'etlMapping.yaml'), 'r') as etl_mapping_file:
         etl_mapping_content = etl_mapping_file.read()
@@ -41,7 +46,8 @@ try:
     data = {
         'user_yaml_content': user_yaml_content,
         'gitops_content': gitops_content,
-        'etl_mapping_content': etl_mapping_content
+        'etl_mapping_content': etl_mapping_content,
+        'css_content': css_content
 
     }
 
