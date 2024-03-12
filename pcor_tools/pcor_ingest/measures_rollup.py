c7a101567b8dd56a9c5ad25ffeca95f0db0ddf7f
Mike Conway <Michael.c.conway@gmail.com>
2024-03-12 16:37:42 -0400

    adds measures lookup

---
+++ pcor_tools/pcor_ingest/measures_rollup.py
@@ -0,0 +1,77 @@
+import logging
+import math
+import re
+import traceback
+import uuid
+import json
+import warnings
+import pandas as pd
+from datetime import datetime
+from pcor_ingest.pcor_intermediate_model import PcorIntermediateProjectModel, \
+    PcorIntermediateResourceModel, PcorIntermediateProgramModel, \
+    PcorSubmissionInfoModel
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s: %(filename)s:%(funcName)s:%(lineno)d: %(message)s"
+
+)
+logger = logging.getLogger(__name__)
+
+
+class PcorMeasuresRollupStructure:
+
+    def __init__(self, parent, subcategory, measure):
+        self.parent = parent
+        self.subcategory = subcategory
+        self.measure = measure
+
+
+class PcorMeasuresRollup:
+
+    def __init__(self, pcor_ingest_configuration):
+        """
+
+        Parameters
+        ----------
+        pcor_ingest_configuration PcorIngestConfiguration that contains configuration from properties file
+        """
+        self.pcor_ingest_configuration = pcor_ingest_configuration
+
+    def measures_rollup_as_dataframe(self):
+        """
+        Get the data frame (pandas) from the measures spreadsheet
+
+        Returns
+        -------
+        pandas.DataFrame representing the contents of the measures rollup
+
+        """
+        logger.info("measures_rollup_as_dataframe")
+        df = pd.read_excel(self.pcor_ingest_configuration.measures_rollup, sheet_name='MeasureList', engine='openpyxl')
+        return df
+
+    def build_measures_structure(self):
+
+        """
+        create a structure by measure that contains the rollup information
+
+        Returns Dictionary with key of measure and value of PcorMeasuresRollup
+        -------
+
+        """
+
+        logger.info("init_measures_structure()")
+        df = self.measures_rollup_as_dataframe()
+        measures_dict = {}
+
+        ss_rows = df.shape[0]
+
+        for i in range(ss_rows):
+            if isinstance(df.iat[i, 0], str):
+                measure = PcorMeasuresRollupStructure(df.iat[i, 0], df.iat[i, 1], df.iat[i, 2])
+                logger.info("measure:%s" % measure)
+                measures_dict[df.iat[i, 2]] = measure
+
+        return measures_dict
+
---
+++ pcor_tools/tests/test_measures_rollup.py
@@ -0,0 +1,33 @@
+import logging
+import unittest
+
+from pcor_ingest.measures_rollup import PcorMeasuresRollup
+from pcor_ingest.pcor_gen3_ingest import PcorGen3Ingest
+from tests import pcor_testing_utilities
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s: %(filename)s:%(funcName)s:%(lineno)d: %(message)s"
+
+)
+logger = logging.getLogger(__name__)
+
+
+class TestPcorGen3Ingest(unittest.TestCase):
+
+    def test_measures_rollup_as_dataframe(self):
+        logger.info('test_measures_rollup_as_dataframe()')
+
+        measures_rollup = PcorMeasuresRollup(pcor_testing_utilities.get_pcor_ingest_configuration())
+
+        df = measures_rollup.measures_rollup_as_dataframe()
+        self.assertIsNotNone(df)
+
+    def test_build_measures_structure(self):
+        measures_rollup = PcorMeasuresRollup(pcor_testing_utilities.get_pcor_ingest_configuration())
+        measures = measures_rollup.build_measures_structure()
+        self.assertIsNotNone(measures)
+        ehr = measures["Electronic Health Records"]
+        self.assertEqual("Electronic Health Records", ehr.measure)
+        self.assertEqual("Health Data", ehr.parent)
+        self.assertEqual('Electronic Health Records', ehr.subcategory)
