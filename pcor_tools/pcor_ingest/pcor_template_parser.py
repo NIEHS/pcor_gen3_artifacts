47db1550948698056ba3317a42b60361d235342c
pateldes <deeppatel.cs@gmail.com>
2024-04-30 09:02:40 -0400

    remove other/Other from props in parsing

--- pcor_tools/pcor_ingest/pcor_template_parser.py
+++ pcor_tools/pcor_ingest/pcor_template_parser.py
@@ -424,15 +424,19 @@ class PcorTemplateParser:
         """
         if main_prop is not None and other_prop is not None:
             combined_prop = main_prop + other_prop
-            if isinstance(combined_prop, str):
-                if "other" in combined_prop.lower():
-                    combined_prop = combined_prop.replace("other", "other, ").replace("Other", "Other, ")
+            if "other" in combined_prop:
+                combined_prop.remove('other')
+            if "Other" in combined_prop:
+                combined_prop.remove('Other')
             return combined_prop
         elif main_prop is None:
             return other_prop
         elif other_prop is None:
+            if "other" in main_prop:
+                main_prop.remove('other')
+            if "Other" in main_prop:
+                main_prop.remove('Other')
             return main_prop
-
 
     @staticmethod
     def formate_date_time(string):
