af948052ec8279719210779b7521f7cf74b76d65
Mike Conway <Michael.c.conway@gmail.com>
2024-05-06 13:24:14 -0400

    discovery refactoring

--- pcor_tools/pcor_ingest/pcor_gen3_ingest.py
+++ pcor_tools/pcor_ingest/pcor_gen3_ingest.py
@@ -253,6 +253,24 @@ class PcorGen3Ingest:
                     tag.category = "Domain"
                     discovery.tags.append(tag)
 
+        filter_project_sponsor_list = [
+            "United States Forestry Service (USFS)",
+            "United States Department of Agriculture (USDOA)",
+            "United States Department of the Interior (USDOI)",
+            "United States Geological Survey (USGS)",
+            "National Aeronautics and Space Administration (NASA)",
+            "United States Environmental Protection Agency (EPA)",
+            "Department of Homeland Security (USDHS)",
+            "Department of Commerce (USDOC)",
+            "Federal Emergency Management Agency (FEMA)",
+            "National Oceanic and Atmospheric Administration (NOAA)",
+            "Center for Disease Control (CDC)",
+            "Department of Health and Human Services (DHHS)",
+            "Agency for Healthcare Research and Quality (AHRQ)",
+            "United States Census Bureau (US Census)",
+            "National Weather Service (NWS)"
+        ]
+        sponsor_no_match = False
         for sponsor in project.project_sponsor:
             if sponsor in filter_project_sponsor_list:
                 search_filter = AdvSearchFilter()
@@ -266,21 +284,7 @@ class PcorGen3Ingest:
             search_filter.key = "Project Sponsor"
             search_filter.value = "Other"
             discovery.adv_search_filters.append(search_filter)
-
-<<<<<<< HEAD
-=======
-        for item in resource.access_type:
-            search_filter = AdvSearchFilter()
-            search_filter.key = "Access Type"
-            search_filter.value = item
-            discovery.adv_search_filters.append(search_filter)
-
-        tag = Tag()
-        tag.name = resource.resource_type
-        tag.category = "Resource Type"
-        discovery.tags.append(tag)
-
->>>>>>> 4a66306 (remove proj sponsor from tags)
+            
         return discovery
 
     def decorate_resc_with_discovery(self, discovery_data):
