543db5fd1089172ed8c283cba422271b75545d3a
pateldes <deeppatel.cs@gmail.com>
2025-01-07 15:06:19 -0500

    #45 refactor CedarLoaderPreprocessor and remove Other entry

--- pcor_tools/pcor_cedar/cedar_loader_preprocessor.py
+++ pcor_tools/pcor_cedar/cedar_loader_preprocessor.py
@@ -38,78 +38,104 @@ class CedarLoaderPreprocessor:
     def process(self, data_model):
         logger.info("process()")
 
-        # collapse project other
+        # Helper function to extend attributes and remove 'other'
+        def extend_and_remove_other(obj, attributes):
+            for attr, other_attr in attributes:
+                primary_list = getattr(obj, attr, None)
+                other_list = getattr(obj, other_attr, None)
+                if primary_list is not None and other_list is not None:
+                    primary_list.extend(other_list)
+                    # Remove 'other/Other' if it exists in the primary list
+                    if 'other' in primary_list:
+                        primary_list.remove('other')
+                    if 'Other' in primary_list:
+                        primary_list.remove('Other')
 
+        # collapse project other
         project = data_model.get("project", None)
         logger.info("processing project %s" % data_model.get("project"))
-        project.project_sponsor.extend(project.project_sponsor_other)
-        project.project_sponsor_type.extend(project.project_sponsor_type_other)
+        extend_and_remove_other(project, [
+            ("project_sponsor", "project_sponsor_other"),
+            ("project_sponsor_type", "project_sponsor_type_other")
+        ])
 
+        # collapse resource props
         resource = data_model.get("resource", None)
         logger.info("processing resource %s" % data_model.get("resource"))
-        resource.domain.extend(resource.domain_other)
+        extend_and_remove_other(resource, [
+            ("domain", "domain_other")
+        ])
 
+        # collapse geo data props
         geospatial_data_resource = data_model.get("geospatial_data_resource", None)
-
         if geospatial_data_resource:
             logger.info("processing geospatial data resource %s" % geospatial_data_resource)
-            geospatial_data_resource.geographic_feature.extend(
-                geospatial_data_resource.geographic_feature_other)
-            geospatial_data_resource.geometry_source.extend(
-                geospatial_data_resource.geometry_source_other)
-            geospatial_data_resource.measurement_method.extend(
-                geospatial_data_resource.measurement_method_other)
-            geospatial_data_resource.measures.extend(
-                geospatial_data_resource.measures_other)
 
-            # add the measures rollup
+            # List of attribute pairs to extend
+            extend_and_remove_other(geospatial_data_resource, [
+                ("geographic_feature", "geographic_feature_other"),
+                ("geometry_source", "geometry_source_other"),
+                ("measurement_method", "measurement_method_other"),
+                ("measures", "measures_other"),
+                ("model_methods", "model_methods_other"),
+                ("spatial_coverage", "spatial_coverage_other"),
+                ("spatial_resolution", "spatial_resolution_other"),
+                ("temporal_resolution", "temporal_resolution_other")
+            ])
+
+            # Add the measures rollup
             measures_rollup = self.pcor_measures_rollup.process_measures(geospatial_data_resource.measures)
             geospatial_data_resource.measures_parent = measures_rollup.measures_parents
             geospatial_data_resource.measures_subcategory_major = measures_rollup.measures_subcategories_major
             geospatial_data_resource.measures_subcategory_minor = measures_rollup.measures_subcategories_minor
 
-            geospatial_data_resource.model_methods.extend(geospatial_data_resource.model_methods_other)
-            geospatial_data_resource.spatial_coverage.extend(geospatial_data_resource.spatial_coverage_other)
-            geospatial_data_resource.spatial_resolution.extend(geospatial_data_resource.spatial_resolution_other)
-            geospatial_data_resource.temporal_resolution.extend(geospatial_data_resource.temporal_resolution_other)
-
-
+        # collapse pop data props
         population_data_resource = data_model.get("population_data_resource", None)
         if population_data_resource:
             logger.info("processing population data resource %s" % population_data_resource)
 
             if population_data_resource.update_frequency_other:
                 population_data_resource.update_frequency.append(population_data_resource.update_frequency_other)
-            population_data_resource.geometry_source.extend(
-                population_data_resource.geometry_source_other)
-            population_data_resource.measures.extend(
-                population_data_resource.measures_other)
-            population_data_resource.model_methods.extend(population_data_resource.model_methods_other)
-            population_data_resource.spatial_coverage.extend(population_data_resource.spatial_coverage_other)
-            population_data_resource.spatial_resolution.extend(population_data_resource.spatial_resolution_other)
-            population_data_resource.temporal_resolution.extend(population_data_resource.temporal_resolution_other)
+            extend_and_remove_other(population_data_resource, [
+                ("geometry_source", "geometry_source_other"),
+                ("measures", "measures_other"),
+                ("model_methods", "model_methods_other"),
+                ("spatial_coverage", "spatial_coverage_other"),
+                ("spatial_resolution", "spatial_resolution_other"),
+                ("temporal_resolution", "temporal_resolution_other")
+            ])
 
+        # collapse geo tool props
         geo_tool_resource = data_model.get("geospatial_tool_resource", None)
         if geo_tool_resource:
             logger.info("processing geospatial tool resource %s" % geo_tool_resource)
-            geo_tool_resource.languages.extend(geo_tool_resource.languages_other)
-            geo_tool_resource.license_type.extend(geo_tool_resource.license_type_other)
-            geo_tool_resource.operating_system.extend(geo_tool_resource.operating_system_other)
-            geo_tool_resource.tool_type.extend(geo_tool_resource.tool_type_other)
+            extend_and_remove_other(geo_tool_resource, [
+                ("languages", "languages_other"),
+                ("license_type", "license_type_other"),
+                ("operating_system", "operating_system_other"),
+                ("tool_type", "tool_type_other")
+            ])
 
+        # collapse key data props
         key_data_resource = data_model.get("key_dataset", None)
         if key_data_resource:
             logger.info("processing key data resource %s" % key_data_resource)
             if key_data_resource.update_frequency_other:
                 key_data_resource.update_frequency.append(key_data_resource.update_frequency_other)
-            key_data_resource.geographic_feature.extend(
-                key_data_resource.geographic_feature_other)
-            key_data_resource.geometry_source.extend(
-                key_data_resource.geometry_source_other)
-            key_data_resource.license_type.extend(key_data_resource.license_type_other)
-            key_data_resource.measurement_method.extend(key_data_resource.measurement_method_other)
-            key_data_resource.measures.extend(
-                key_data_resource.measures_other)
+                extend_and_remove_other(key_data_resource, [
+                    ("geographic_feature", "geographic_feature_other"),
+                    ("geometry_source", "geometry_source_other"),
+                    ("license_type", "license_type_other"),
+                    ("measurement_method", "measurement_method_other"),
+                    ("measures", "measures_other"),
+                    ("model_methods", "model_methods_other"),
+                    ("spatial_coverage", "spatial_coverage_other"),
+                    ("spatial_resolution", "spatial_resolution_other"),
+                    ("spatial_resolution_all_available", "spatial_resolution_all_other_available"),
+                    ("temporal_resolution", "temporal_resolution_other"),
+                    ("temporal_resolution_all_available", "temporal_resolution_all_other_available"),
+                    ("use_suggested", "use_suggested_other")
+                ])
 
             # add the measures rollup
             measures_rollup = self.pcor_measures_rollup.process_measures(key_data_resource.measures)
@@ -117,13 +143,4 @@ class CedarLoaderPreprocessor:
             key_data_resource.measures_subcategory_major = measures_rollup.measures_subcategories_major
             key_data_resource.measures_subcategory_minor = measures_rollup.measures_subcategories_minor
 
-            key_data_resource.model_methods.extend(key_data_resource.model_methods_other)
-            key_data_resource.spatial_coverage.extend(key_data_resource.spatial_coverage_other)
-            key_data_resource.spatial_resolution.extend(key_data_resource.spatial_resolution_other)
-            key_data_resource.spatial_resolution_all_available.extend(
-                key_data_resource.spatial_resolution_all_other_available)
-            key_data_resource.temporal_resolution.extend(key_data_resource.temporal_resolution_other)
-            key_data_resource.temporal_resolution_all_available.extend(
-                key_data_resource.temporal_resolution_all_other_available)
-            key_data_resource.use_suggested.extend(
-                key_data_resource.use_suggested_other)
+
